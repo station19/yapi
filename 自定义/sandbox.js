bbd833b357061f347d8cf70ba88fc21490f6bb15
station19 <wuyuzai@qq.com>
2023-11-09 14:24:00 +0800

    Add files via upload

---
+++ 自定义/sandbox.js
@@ -0,0 +1,37 @@
+const Safeify = require('safeify').default;
+
+module.exports = async function sandboxFn(context, script) {
+    // 创建 safeify 实例
+    const safeVm = new Safeify({
+        timeout: 3000,
+        asyncTimeout: 60000,
+	unrestricted: true,
+	unsafe: {
+    		modules: {
+        		assert: 'assert'
+    		}
+	}
+    });
+    safeVm.preset('const assert = require("assert");');
+    //script += "; return this;";
+    script += `; return {
+        Function: this.Function,
+        eval: this.eval,
+        header: this.header,
+        query: this.query,
+        body: this.body,
+        mockJson: this.mockJson,
+        params: this.params,
+        resHeader: this.resHeader,
+        httpCode: this.httpCode,
+        delay: this.delay,
+        Random: this.Random,
+        cookie: this.cookie
+    }`;
+    // 执行动态代码
+    const result = await safeVm.run(script, context)
+
+    // 释放资源
+    safeVm.destroy()
+    return result
+}
